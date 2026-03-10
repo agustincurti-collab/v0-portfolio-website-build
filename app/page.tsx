6112373c98c96fb80052fcaeefeeefb59d66a9b8
agustincurti-collab <agustin.curti@gmail.com>
2026-03-10 20:07:08 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
     <main className="min-h-screen bg-background">
       <Header />
       <Hero />
-      <Logos />
+      <Tech />
       <Work />
       <Services />
       <Process />
