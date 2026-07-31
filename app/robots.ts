74a3f1292451bbfeb72a5bb8195303075ff65b8a
agustincurti-collab <agustin.curti@gmail.com>
2026-07-31 20:47:30 -0300

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -1,6 +1,6 @@
 import type { MetadataRoute } from 'next'
 
-const SITE_URL = 'https://agustincurti.com'
+const SITE_URL = 'https://agustincurti.com.ar'
 
 export default function robots(): MetadataRoute.Robots {
   return {
