d86151fc9cdc4936c900579680e271d4c90b0d1c
agustincurti-collab <agustin.curti@gmail.com>
2026-07-31 20:47:14 -0300

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,6 @@
 import type { MetadataRoute } from 'next'
 
-const SITE_URL = 'https://agustincurti.com'
+const SITE_URL = 'https://agustincurti.com.ar'
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
