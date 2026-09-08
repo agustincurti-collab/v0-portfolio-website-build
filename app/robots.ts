f813b32332a39985fbfb552f097338d40d0fd96b
agustincurti-collab <agustin.curti@gmail.com>
2026-09-08 10:44:54 -0300

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,8 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
+      // Las propuestas son privadas / para un cliente puntual: no se indexan
+      disallow: ['/propuestas', '/propuesta'],
     },
     sitemap: `${SITE_URL}/sitemap.xml`,
   }
